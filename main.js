bb414930e42613cd4c923d4ba82b1b57f71f8c8b
ros9523 <rofidiaz33@gmail.com>
2022-07-14 17:22:50 -0600

    Added the Portafolio

---
+++ main.js
@@ -0,0 +1,10 @@
+const iconMenu = document.querySelector("#icon__burger");
+const locateMenu = document.querySelector("#menu");
+
+iconMenu.addEventListener("click", function () {
+locateMenu.classList.toggle("show__menu");
+});
+
+
+
+
